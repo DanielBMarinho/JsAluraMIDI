df59e15a5b40b1310cc69df0bcd32072cf62de0b
Daniel B. Marinho <barbosamarinhodaniel@gmail.com>
2024-09-20 14:02:48 -0300

    arquivos javascrip

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,37 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    if (elemento === null) {
+        console.log ('Elemento não encontrado');
+    }
+
+    if(elemento != null) {
+        if(elemento.localName === 'audio') {
+        elemento.play();  
+        }
+        
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas [contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;//tamplate String
+
+    tecla.onclick =function () {
+        tocaSom (idAudio);
+    }
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
